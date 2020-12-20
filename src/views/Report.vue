725a47e572f58d0b419988bad91a57b8e3da927a
Dawit <27751688+oneminch@users.noreply.github.com>
2020-12-20 02:54:24 -0600

    feature: add basic pages to routes: Submit and Report

--- src/views/Report.vue
+++ src/views/Report.vue
@@ -1,24 +1,79 @@
 <template>
     <div id='report'>
-        <h2>This is a report page</h2>
-        
+        <h3>Report Page</h3>
+        <p>
+            There are also some services that don't currently have such encryption but plan to itegrate it into their products. 
+            These services either have encryption as a beta feature or have it on their roadmap.
+            They can be accessed here.
+            They are also accessible from the "Pages" dropdown in the navigation menu under "report". 
+        </p> 
+        <div class="product-list">
+			<app-product 
+				v-for="(product, index) in allProducts" 
+				:key="index" 
+				:product="product"
+				:class="{ 'last-product' : (index === allProducts.length - 1)}"
+			></app-product>
+		</div>      
     </div>
 </template>
 
 
 <script>
-
 export default {
-    name: 'Report'
+    name: 'Report',
+    data() {
+        return {}
+    },
+    methods: {
+        // fetches and populates list from db
+        populateList: function() {
+            // fetch and populate
+        }
+    },
+    beforeRouteEnter(to, from, next) {
+        console.log(to);
+        console.log(from);
+        next();
+    }
 }
 </script>
 
 
 <style scoped>
-
 * {
     outline: none;
 }
 
+#report {
+    padding: .5rem;
+}
 
+#report p {
+    width: 80%;
+    text-align: center;
+    margin: 0 auto;
+}
+
+.product-list {
+	width: 85%;
+    margin: 2rem auto;
+}
+
+@media only screen and (max-width: 768px) {
+    .product-list {
+        width: 100%;
+    }
+}
+
+@media only screen and (max-width: 480px) {
+	#report {
+        padding: 1rem .25rem;
+	}
+
+    #report p {
+        width: 95%;
+        text-align: justify;
+	}
+}
 </style>
--- src/views/Submit.vue
+++ src/views/Submit.vue
@@ -1,14 +1,33 @@
 <template>
     <div id='submit'>
-        <h2>This is a submission page</h2>
+        <h3>Submission Page</h3>
+        <p>
+            There are also some services that don't currently have such encryption but plan to itegrate it into their products. 
+            These services either have encryption as a beta feature or have it on their roadmap.
+            They can be accessed here.
+            They are also accessible from the "Pages" dropdown in the navigation menu under "submit". 
+        </p>     
     </div>
 </template>
 
 
 <script>
-
 export default {
-    name: 'Submit'
+    name: 'Submit',
+    data() {
+        return {}
+    },
+    methods: {
+        // fetches and populates list from db
+        populateList: function() {
+            // fetch and populate
+        }
+    },
+    beforeRouteEnter(to, from, next) {
+        console.log(to);
+        console.log(from);
+        next();
+    }
 }
 </script>
 
@@ -16,5 +35,37 @@ export default {
 <style scoped>
 * {
     outline: none;
+}
+
+#submit {
+    padding: .5rem;
+}
+
+#submit p {
+    width: 80%;
+    text-align: center;
+    margin: 0 auto;
+}
+
+.product-list {
+	width: 85%;
+    margin: 2rem auto;
+}
+
+@media only screen and (max-width: 768px) {
+    .product-list {
+        width: 100%;
+    }
+}
+
+@media only screen and (max-width: 480px) {
+	#submit {
+        padding: 1rem .25rem;
+	}
+
+    #submit p {
+        width: 95%;
+        text-align: justify;
+	}
 }
 </style>
