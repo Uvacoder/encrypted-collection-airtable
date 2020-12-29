527f0fd8462cd0c06f5b5b018d1b80a447ef475f
Dawit <27751688+oneminch@users.noreply.github.com>
2020-12-29 01:38:22 -0600

    import icon CSS using <link> instead of import to improve performance

--- src/App.vue
+++ src/App.vue
@@ -74,7 +74,7 @@ export default {
 
 <style>
 /* Import all the CSS icons needed from https://css.gg/ */
-@import url('https://css.gg/css?=|arrow-up|search|backspace|asterisk|link|external|corner-right-up|chevron-down-o|menu|sun|moon|add|danger|info|unavailable|eye|push-left|push-right|smile-mouth-open|close');
+/* @import url('https://css.gg/css?=|arrow-up|search|backspace|asterisk|link|external|corner-right-up|chevron-down-o|menu|sun|moon|add|danger|info|unavailable|eye|push-left|push-right|smile-mouth-open|close'); */
 
 /* Import regular monospace font */
 @font-face {
