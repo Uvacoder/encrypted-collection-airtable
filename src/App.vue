c15943a972286c404e24be4d48ef4690ed267b68
Dawit <27751688+oneminch@users.noreply.github.com>
2021-01-17 22:41:14 -0600

    improve order of SEO keywords

--- src/App.vue
+++ src/App.vue
@@ -32,7 +32,7 @@ export default {
 			{ charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1'},
 			{ name: 'description', content: 'EncryptedList is a collection of products & services that offer or ensure data security through zero-knowledge or end-to-end encryption.'},
-			{ name: 'keywords', content: 'list, collection, encrypted apps, encrypted, privacy, security, encryption, end-to-end encryption, zero-knowledge encryption' },
+			{ name: 'keywords', content: 'encryption, end-to-end encryption, zero-knowledge encryption, encrypted apps, encrypted, privacy, security, list, collection' },
 
 			// OpenGraph data
 			{ property: 'og:type', content: 'website'},
