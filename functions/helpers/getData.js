9142221bd899e35f72d7ba7da7ecff2266400426
Dawit <27751688+oneminch@users.noreply.github.com>
2021-02-17 12:22:23 -0600

    remove api guards for production

--- functions/helpers/getData.js
+++ functions/helpers/getData.js
@@ -5,11 +5,9 @@ const formatReturn = require("./formatReturn");
 module.exports = async (tableName, numRecs) => {
 	try {
 		const table = base(tableName);
-		const baseImgURL = `https://ik.imagekit.io/x6xq2u8ftjl/encryptedlist`;
 		const initialLimit = typeof numRecs !== "undefined" ? numRecs : 150;
 
-		let products = [],
-			imgName = ``;
+		let products = [];
 
 		const recs = await table
 			.select({
@@ -18,11 +16,6 @@ module.exports = async (tableName, numRecs) => {
 			})
 			.eachPage((records, fetchNextPage) => {
 				records.forEach((record) => {
-					imgName = `${record.fields.name
-						.split(" ")
-						.join("-")
-						.toLowerCase()}`;
-
 					products.push({
 						name: record.fields.name,
 						desc: record.fields.description,
@@ -32,9 +25,6 @@ module.exports = async (tableName, numRecs) => {
 						alternatives: record.fields.alternatives
 							? record.fields.alternatives
 							: [],
-						// png_img: `${baseImgURL}/png/${imgName}.png?tr=w-105,dpr-1,lo-true`,
-						// webp_img: `${baseImgURL}/png/${imgName}.png?tr=w-105,dpr-1,lo-true,f-webp`,
-						// webp_img: `${baseImgURL}/webp/${imgName}.webp?tr=w-105,dpr-1,lo-true`,
 					});
 				});
 				fetchNextPage();
