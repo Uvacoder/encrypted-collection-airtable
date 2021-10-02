15bc293b752cc135c401aef2d16027dd66e67eb9
Dawit <27751688+oneminch@users.noreply.github.com>
2021-10-02 06:11:50 -0500

    Add Ko-fi button

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -114,6 +114,8 @@
 						@clicked="toggleMoreCategories"
 					></app-button-tag>
 				</div>
+				
+				<script type='text/javascript' src='https://storage.ko-fi.com/cdn/widget/Widget_2.js'></script><script type='text/javascript'>kofiwidget2.init('Buy Me Coffee', '#f1c61b', 'R5R76FGFT');kofiwidget2.draw();</script> 
 			</div>
 		</div>
 	</div>
