ae3c31340a756806a3a49b811ade76cbf5d13e4e
Dawit <27751688+oneminch@users.noreply.github.com>
2022-01-22 22:19:50 -0600

    Remove 'Buy me coffee' button

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -103,15 +103,6 @@
 						@clicked="toggleMoreCategories"></app-button-tag>
 				</div>
 			</div>
-
-			<a href="https://ko-fi.com/R5R76FGFT" target="_blank"
-				><img
-					height="36"
-					style="border: 0px; height: 36px"
-					src="https://cdn.ko-fi.com/cdn/kofi2.png?v=3"
-					border="0"
-					alt="Buy Me a Coffee at ko-fi.com"
-			/></a>
 		</div>
 	</div>
 </template>
